89c85a64615d8f13442f31e64ea61d5174133a4c
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2024-07-12 12:01:56 +0300

    send mail in tasks
    Signed-off-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>

--- leave-request/tasks/send-approved-notification.ts
+++ leave-request/tasks/send-approved-notification.ts
@@ -1,1 +1,17 @@
-console.log("Request approved!");
+import { process } from "sdk/bpm"
+import { sendMail } from "./mail-util"
+
+const execution = process.getExecutionContext();
+const executionId = execution.getId();
+
+const processVariables = process.getVariables(executionId);
+
+const fromDate = processVariables.fromDate;
+const toDate = processVariables.toDate;
+const approver = processVariables.approver;
+const requester = processVariables.fromDate;
+
+const subject = "Your leave request has been approved";
+const content = `<h4>Your leave request from [${fromDate}] to [${toDate}] has been approved by [${approver}] has been created</h4>`;
+
+sendMail(requester, subject, content)
--- leave-request/tasks/send-declined-notification.ts
+++ leave-request/tasks/send-declined-notification.ts
@@ -1,1 +1,17 @@
-console.log("Request declined!");
+import { process } from "sdk/bpm"
+import { sendMail } from "./mail-util"
+
+const execution = process.getExecutionContext();
+const executionId = execution.getId();
+
+const processVariables = process.getVariables(executionId);
+
+const fromDate = processVariables.fromDate;
+const toDate = processVariables.toDate;
+const approver = processVariables.approver;
+const requester = processVariables.fromDate;
+
+const subject = "Your leave request has been declined";
+const content = `<h4>Your leave request from [${fromDate}] to [${toDate}] has been declined by [${approver}] has been created</h4>`;
+
+sendMail(requester, subject, content)
